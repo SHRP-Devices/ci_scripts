40485e8da0b7d0f2b3b447e7c658e80b80dfeb08
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-05-23 21:05:35 +0200

    ci: onclite: remove extra useless flags

--- onclite.sh
+++ onclite.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_onclite.git -b android-9.0 --depth=1 device/xiaomi/onclite
-export ALLOW_MISSING_DEPENDENCIES=true
 source build/envsetup.sh
 lunch omni_onclite-eng
-mka recoveryimage -j$(nproc --all)
+mka recoveryimage
