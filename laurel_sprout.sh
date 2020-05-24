1ba8f6a6c8636fd487ab7536ec1df414ef6aab79
oddlyspaced <oddlyspaced1@gmail.com>
2020-05-24 20:00:33 +0530

    laurel_sprout: Track shrp devices repo instead of personal one

--- laurel_sprout.sh
+++ laurel_sprout.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/LineageOS/android_hardware_qcom_bootctrl.git -b lineage-16.0 hardware/qcom/bootctrl
-git clone https://github.com/oddlyspaced/shrp_xiaomi_laurel_sprout.git device/xiaomi/laurel_sprout
+git clone https://github.com/SHRP-Devices/device_xiaomi_laurel_sprout.git device/xiaomi/laurel_sprout
 . build/envsetup.sh
 lunch omni_laurel_sprout-eng
 mka bootimage
