a17eb37b74ddebf62c135ca16b9197eefef5cf2f
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-17 11:24:42 +0200

    ci: fixup c103 path

--- c103.sh
+++ c103.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/SHRP-Devices/device_coolpad_c103.git -b treble
+git clone https://github.com/SHRP-Devices/device_coolpad_c103.git -b treble device/coolpad/c103
 . build/envsetup.sh
 lunch omni_c103-eng
 mka recoveryimage
