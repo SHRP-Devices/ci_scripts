7280e0c0ca9fc0dcf85c0649693d04a5728e46c8
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-17 11:28:43 +0200

    ci: fixup rosy path

--- rosy.sh
+++ rosy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/SHRP-Devices/recovery-device-xiaomi-rosy.git
+git clone https://github.com/SHRP-Devices/recovery-device-xiaomi-rosy.git device/xiaomi/rosy
 . build/envsetup.sh
 lunch omni_rosy-eng
 mka recoveryimage
