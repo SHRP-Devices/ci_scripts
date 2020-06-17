dabb9dfea586e76cd05b367228b188cd2128bd54
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-17 12:15:18 +0200

    ci: daisy: add qcom-common

--- daisy.sh
+++ daisy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_daisy_shrp device/xiaomi/daisy
+git clone https://github.com/TeamWin/android_device_qcom_common -b android-9.0 device/qcom/common
 . build/envsetup.sh
 lunch omni_daisy-eng
 mka recoveryimage
