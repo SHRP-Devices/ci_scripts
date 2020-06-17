0d3ba9c45c4266860d91a722c033559b0303d332
Hadad Darajat <hadaddarajat@yahoo.com>
2020-06-17 08:19:04 +0700

    ci: update recovery tree for onclite (#37)
    * Update recovery tree location on GitHub
    * Delete specific cloning branch
    
    Signed-off-by: hadaddarajat <hadaddarajat@yahoo.com>

--- onclite.sh
+++ onclite.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/SHRP-Devices/device_xiaomi_onclite.git -b android-9.0 --depth=1 device/xiaomi/onclite
+git clone https://github.com/SHRP-Devices/android_device_xiaomi_onclite.git --depth=1 device/xiaomi/onclite
 source build/envsetup.sh
 lunch omni_onclite-eng
 mka recoveryimage
