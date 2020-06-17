5de7af758e481e41c1a50c51c26901ef2f1456fd
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-17 21:47:52 +0200

    ci: onclite: clone qcom-commonsys

--- onclite.sh
+++ onclite.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/android_device_xiaomi_onclite.git --depth=1 device/xiaomi/onclite
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 source build/envsetup.sh
 lunch omni_onclite-eng
 mka recoveryimage
