0331a5d9cae4ab5f4089ca1768cb0a59f3b98b56
Giovix92 <ggualtierone@gmail.com>
2020-06-16 21:32:56 -0400

    ci: fix ysl path

--- ysl.sh
+++ ysl.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/SHRP-Devices/Device_xiaomi_ysl.git
+git clone https://github.com/SHRP-Devices/Device_xiaomi_ysl.git device/xiaomi/ysl
 . build/envsetup.sh
 lunch omni_ysl-eng
 mka recoveryimage
