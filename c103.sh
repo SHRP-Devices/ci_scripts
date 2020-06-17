87ac097e9e94b4292129157785460ff96d1fbeaf
epicX67 <46811948+epicX67@users.noreply.github.com>
2020-06-17 16:17:14 +0530

    clone qcom/opensource/commonsys

--- c103.sh
+++ c103.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_coolpad_c103.git -b treble device/coolpad/c103
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_c103-eng
 mka recoveryimage
