5d729cd3275efb5ad14c39cc08fd47537b942f35
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-17 20:29:31 +0200

    ci: lavender: clone qcom-commonsys

--- lavender.sh
+++ lavender.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_lavender device/xiaomi/lavender
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_lavender-eng
 mka recoveryimage
