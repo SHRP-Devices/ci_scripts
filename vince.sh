b65e06bee1ad740eafd80bd29db2e1a8347a70cd
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-18 12:42:49 +0200

    ci: vince: clone qcom-commonsys

--- vince.sh
+++ vince.sh
@@ -1,4 +1,4 @@
 #!bin/bash
 git clone https://github.com/SHRP-Devices/recovery_device_xiaomi_vince device/xiaomi/vince
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 export ALLOW_MISSING_DEPENDENCIES=true; source build/envsetup.sh; lunch omni_vince-eng; mka recoveryimage;
-echo "vincememe is done"
