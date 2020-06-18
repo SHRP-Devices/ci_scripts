0c0839ac5cc4e6b28db746b77b88eec4784daf2d
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-18 12:56:49 +0200

    ci: xmsirius: clone qcom-commonsys

--- xmsirius.sh
+++ xmsirius.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/android_device_xiaomi_xmsirius device/xiaomi/xmsirius
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_xmsirius-eng
 mka recoveryimage
