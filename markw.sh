c7e280ce878baaa54144ff0a9b2dee2cc8e0e70b
Giovix92 <ggualtierone@gmail.com>
2020-06-19 06:07:48 -0400

    ci: markw/rosy: add qcom-commonsys

--- markw.sh
+++ markw.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_markw_shrp.git device/xiaomi/markw
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_markw-eng
 mka recoveryimage
