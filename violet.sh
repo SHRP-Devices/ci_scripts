dc118be629892d9b2222376d6e9bfc3e7f9e3155
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-20 16:10:32 +0200

    ci: violet: clone qcom-commonsys

--- violet.sh
+++ violet.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_violet device/xiaomi/violet
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_violet-eng
 mka recoveryimage
