cfbe1d8bbc55c9c48aebbefab1ebf8da8ce51b81
Giovanni Gualtieri <ggualtierone@gmail.com>
2020-06-20 16:14:43 +0200

    ci: riva: clone qcom-commonsys

--- riva.sh
+++ riva.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_riva device/xiaomi/riva
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_riva-eng
+rm -rf device/qcom*
 mka recoveryimage
