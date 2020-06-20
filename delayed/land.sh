7e9ed2d06e6ed2989bcc50231841578595a46e35
sreeharimkl <50312757+sreeharimkl@users.noreply.github.com>
2020-06-20 19:36:08 +0530

    ci: add qcom-common (#41)

--- delayed/land.sh
+++ delayed/land.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_xiaomi_land device/xiaomi/land
+git clone https://github.com/TeamWin/android_device_qcom_common -b android-9.0 device/qcom/common
 . build/envsetup.sh
 lunch omni_land-eng
 mka recoveryimage
