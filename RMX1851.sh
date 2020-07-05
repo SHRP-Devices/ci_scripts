b09a432a0291d8f592cb928dc416ce3a6e27e941
ahmedmoselhi <ahmedmoselhi55@gmail.com>
2020-07-06 00:52:42 +0200

    Update RMX1851.sh

--- RMX1851.sh
+++ RMX1851.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-git clone https://github.com/SHRP-Devices/kernel_realme_rmx1851.git kernel/realme/sdm710
 git clone https://github.com/SHRP-Devices/device_realme_RMX1851.git device/realme/RMX1851
 . build/envsetup.sh
 lunch omni_RMX1851-eng
