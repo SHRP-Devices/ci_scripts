f7ffff1ba007c7fac7516847f906e95eb26bfe8b
rico192 <ricoananda.9c26@gmail.com>
2020-07-07 19:24:17 -0700

    ci: add qcom/opensource/commonsys for oneplus3

--- rosy.sh
+++ rosy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/recovery-device-xiaomi-rosy.git
+git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
 . build/envsetup.sh
 lunch omni_rosy-eng
 mka recoveryimage
