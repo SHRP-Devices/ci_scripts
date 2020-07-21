545cb703c938dcb1eebdfe6ffc482163e48099fb
Griffin <bluedogerino@gmail.com>
2020-07-21 14:53:01 +0200

    ci: change j5y17lte repo to shrp-devices one (#51)

--- j5y17lte.sh
+++ j5y17lte.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/bluedogerino/shrp_j5y17lte device/samsung/j5y17lte
+git clone https://github.com/SHRP-Devices/device_samsung_j5y17lte.git device/samsung/j5y17lte
 . build/envsetup.sh
 lunch omni_j5y17lte-eng
 mka recoveryimage
