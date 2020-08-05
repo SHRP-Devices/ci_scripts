f8ad4a5e93ff0b951903c8d3aebcd2acf32aea1b
marchetto94 <52181172+marchetto94@users.noreply.github.com>
2020-08-05 11:22:38 -0300

    j7y17lte.sh

--- j7y17lte.sh
+++ j7y17lte.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/marchetto94/device_samsung_j7y17lte device/samsung/j7y17lte
+git clone https://github.com/marchetto94/android_device_samsung_j7y17lte device/samsung/j7y17lte
 . build/envsetup.sh
 lunch omni_j7y17lte-eng
 mka recoveryimage
