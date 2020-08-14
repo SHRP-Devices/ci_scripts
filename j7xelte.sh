d7c95022693a210ae9c7529af5d13b44bca8f967
marchetto94 <eric.chavess@gmail.com>
2020-08-14 18:10:23 -0300

    ci: add bunch of Samsung devices

--- j7xelte.sh
+++ j7xelte.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git clone https://github.com/marchetto94/android_device_skyhawk -b j7y17lte device/samsung/j7y17lte
+git clone https://github.com/marchetto94/android_device_skyhawk -b j7xelte device/samsung/j7xelte
 git clone https://gitlab.com/Universal7870/common/android_kernel_samsung_exynos7870.git -b skyhawk kernel/samsung/exynos7870
 . build/envsetup.sh
-lunch omni_j7y17lte-eng
+lunch omni_j7xelte-eng
 mka recoveryimage
