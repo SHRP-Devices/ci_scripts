84cd80daf496d0a4da0c4ae4968ad35f1d217a23
Imdeepak0001 <63868546+Imdeepak0001@users.noreply.github.com>
2020-10-11 19:23:01 +0530

    Update RMX1831.sh

--- RMX1831.sh
+++ RMX1831.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/Imdeepak0001/android_device_realme_RMX1831
+git clone https://github.com/SHRP-Devices/device_realme_RMX1831
 . build/envsetup.sh
 lunch omni_RMX1831-eng
 mka recoveryimage
