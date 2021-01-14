bdc9bd45d8a107750f920495f1e582efe01c5cfe
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-14 17:58:14 +0100

    ci_scripts: Drop Samsung MSM8917 family

--- j4primelte.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/SHRP-Devices/device_samsung_msm8917-common device/samsung/msm8917-common
-git clone https://github.com/SHRP-Devices/device_samsung_j4primelte device/samsung/j4primelte
-. build/envsetup.sh
-lunch omni_j4primelte-eng
-mka recoveryimage
