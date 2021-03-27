82adc9bd4c2e9099ff69182472d63d8825058969
Yaroslav Popkov <yarikpopkov1@gmail.com>
2021-03-27 13:44:24 +0500

    Added cactus and cereus

---
+++ cactus.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/SHRP-Devices/device_xiaomi_cactus
+. build/envsetup.sh
+lunch omni_cactus-eng
+mka recoveryimage
---
+++ cereus.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/SHRP-Devices/device_xiaomi_cactus
+. build/envsetup.sh
+lunch omni_cereus-eng
+mka recoveryimage
