881fa80caa269c2d954acdd70e2510d11b01880d
steadfasterX <steadfasterX@users.noreply.github.com>
2021-04-21 11:10:25 +0200

    j5y17lte: change to new repo

--- j5y17lte.sh
+++ j5y17lte.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/SHRP-Devices/device_samsung_j5y17lte.git device/samsung/j5y17lte
-. build/envsetup.sh
+git clone https://github.com/SHRP-Devices/android_device_samsung_j5y17lte.git device/samsung/j5y17lte -b android-9.0_shrp
+source build/envsetup.sh
 lunch omni_j5y17lte-eng
 mka recoveryimage
