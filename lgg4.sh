d2be0a7ac2484bf63dd61df479f73ad8db7fe3d5
steadfasterX <steadfasterX@users.noreply.github.com>
2021-04-21 10:44:59 +0200

    new kernel repo

--- lgg4.sh
+++ lgg4.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_lge_g4.git device/lge/g4 -b android-9.0_shrp
-git clone https://github.com/Suicide-Squirrel/Titan-Kernel-LG-G4.git kernel/lge/g4 -b android-9.0_sfx
+git clone https://github.com/LGgFour/kernel_lge_msm8992.git  kernel/lge/g4 -b android-9.0
 source build/envsetup.sh
 lunch omni_lg4-eng
 mka recoveryimage
