7286cf9e0c5c1f709989d690dbafb6e25bf0de60
Velosh <daffetyxd@gmail.com>
2021-05-01 18:54:14 -0300

    a20s: Fetch kernel tree + Minor change
    Signed-off-by: Velosh <daffetyxd@gmail.com>

--- a20s.sh
+++ a20s.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 git clone https://github.com/SHRP-Devices/device_samsung_a20s device/samsung/a20s
-. build/envsetup.sh
+git clone https://github.com/a2XX-dev/android_kernel_samsung_a20s -b sky kernel/samsung/a20s
+source build/envsetup.sh
 lunch omni_a20s-eng
 mka recoveryimage
