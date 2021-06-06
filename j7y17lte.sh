6ca41c076d20b0a874f3ece2cd2fbb6c8ee8eaaf
Pietro Lotto <52181172+PietroLotto@users.noreply.github.com>
2021-06-06 18:59:51 -0300

    j7y17lte: change to new repo (#79)

--- j7y17lte.sh
+++ j7y17lte.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git clone https://github.com/marchetto94/android_device_skyhawk -b j7y17lte device/samsung/j7y17lte
+git clone https://github.com/pietrolotto/android_device_skyhawk.git -b j7y17lte device/samsung/j7y17lte
 git clone https://gitlab.com/Universal7870/common/android_kernel_samsung_exynos7870.git -b skyhawk kernel/samsung/exynos7870
 . build/envsetup.sh
 lunch omni_j7y17lte-eng
