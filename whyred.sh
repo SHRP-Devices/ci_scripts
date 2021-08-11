7218e23bd5ed32f0ac0a0d01b51a2295fddd470f
Mikesew1320 (Inspired by VR25 at XDA) <43565227+Mikesew1320@users.noreply.github.com>
2021-08-11 11:11:29 +0300

    Delete whyred.sh

--- whyred.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone https://github.com/SHRP-Devices/device_xiaomi_whyred device/xiaomi/whyred
-git clone https://github.com/omnirom/android_vendor_qcom_opensource_commonsys -b android-9.0 vendor/qcom/opensource/commonsys
-. build/envsetup.sh
-lunch omni_whyred-eng
-mka recoveryimage
