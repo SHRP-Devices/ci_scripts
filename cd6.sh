86246924c5dbd7a21060cf8fa30abbea6b99712b
Mikesew1320 <mikesew1320@gmail.com>
2021-08-13 16:09:05 +0300

    CD6: Fix Repo Link

--- cd6.sh
+++ cd6.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # Fetch device tree
-git clone https://github.com/SHRP-Devices/TECNO-CD6-SHRP TECNO-CD6-SHRP
+git clone https://github.com/SHRP-Devices/TECNO-CD6-SHRP device/TECNO/TECNO_Camon_15_Air
 
 # Setup the build
 . build/envsetup.sh
 lunch omni_CD6-eng
 
 # Actual build
-mka recoveryimage
